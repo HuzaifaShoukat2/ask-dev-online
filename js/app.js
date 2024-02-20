45517e4e237295735cc3074c33e2d58dd2c94619
Huzaifa Shoukat Ali <huzaifashoukat11@gmail.com>
2024-02-20 19:45:59 +0500

    commit

--- js/app.js
+++ js/app.js
@@ -3,5 +3,21 @@ function menuToggle() {
     toggleMenu.classList.toggle("active");
   }
   
+document.addEventListener("DOMContentLoaded", function () {
+  const switchSignin = document.querySelector(".switchSignin");
+  const switchSignUp = document.querySelector(".switchSignup");
+  const switchSigIn = () => {
+    document.querySelector(".signin-form").classList.remove("d-none");
+    document.querySelector(".signin-form").classList.add("d-block");
+    document.querySelector(".signup-form").classList.add("d-none");
+  };
+  const switchSignup = () => {
+    document.querySelector(".signup-form").classList.remove("d-none");
+    document.querySelector(".signup-form").classList.add("d-block");
+    document.querySelector(".signin-form").classList.add("d-none");
+  };
+  switchSignUp && switchSignUp.addEventListener("click", switchSignup);
+  switchSignin && switchSignin.addEventListener("click", switchSigIn);
+});
 
   
